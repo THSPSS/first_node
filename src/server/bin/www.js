04277c9948fbb6f23fba006be73a8e3d1c30191b
[THSPSS] <[sunyeongbang98@gmail.com]>
2023-02-10 16:24:07 +0900

    포트번호바꿈

--- src/server/bin/www.js
+++ src/server/bin/www.js
@@ -3,7 +3,7 @@ import "dotenv/config";
 import "regenerator-runtime";
 import app from "../app";
 
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () =>
   console.info(`Server Open :::::: http://localhost:${PORT}`)
